21caba6c3cec9631b8b34dcdde847a3117504bd5
Yonier Gomez <yasprill@bancolombia.com.co>
2023-09-18 09:38:33 -0500

    Mi primer paquete de harry potter

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from yonier_harrypotter import harrypotter
+import requests, logging
 
+def harrypotter():
+    url = 'https://hp-api.onrender.com/api/characters'
+    response = requests.get(url)
 
+    if response.ok:
+        payload = response.json()
+        for r in range(len(payload)):
+            print(f"Hola, soy {payload[r]['name']} y mi casa es: {payload[r]['house']} " )
+
+logging.basicConfig(level=logging.DEBUG)
 if __name__ == '__main__':
-    print('Hola, esta es una prueba pythonica')
-    harrypotter.harrypotter()
+    logging.debug('Obteniendo los nombre y casas de los personajes de Harry Potter')
+    harrypotter()
+    logging.debug('La tarea ha finalizado correctamente')
